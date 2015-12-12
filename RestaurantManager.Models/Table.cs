f4f764c3f9ba61443c859a4bfce25519abc18915
EricEDX <yodenai-git@yahoo.fr>
2015-12-12 12:08:49 +0100

    Homework Module 2
    Homework Assignment 2

---
+++ RestaurantManager.Models/Table.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManager.Models
+{
+    public class Table
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return Description.ToString();
+        }
+    }
+}
